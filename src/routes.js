5f371f745af55f3f3a8061bedd15a5e026d3189b
Fellipe Rezende <felliperezende@iMac-de-Fellipe.local>
2020-03-05 15:26:33 -0300

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      defaultNavigationOptions: {
+        headerBackTitleVisible: false,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#6ba8a9',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
+);
+
+export default Routes;
